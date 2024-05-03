5ae617e20ed0bc7c805c7aacf1aff58b9c8acaf4
Derek Marion <derek.j.marion@gmail.com>
2024-05-03 11:19:44 -0700

    Add print statement to test htmlnode class repr

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 
 def main():
     textnode = TextNode("This is a text node", "bold", "https://example.com")
     print(textnode)
+
+    htmlnode = HTMLNode("a", "this is a link", "children", {"href": "https://example.com"})
+    print(htmlnode)
 
 
 if __name__ == "__main__":
