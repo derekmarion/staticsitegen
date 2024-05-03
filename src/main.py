150f9facc163889a8b999a4c4eab169a72fcee8d
Derek Marion <derek.j.marion@gmail.com>
2024-05-03 09:47:02 -0700

    Add project skeleton, textnode class with tests

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    textnode = TextNode("This is a text node", "bold", "https://example.com")
+    print(textnode)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_1(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_2(self):
+        node = TextNode("This is a text node", "bold", "https://some-url.com")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
